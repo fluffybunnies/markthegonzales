43e89289060474762198fcccf7708c9c1b6c1842
alec <alec.hulce@gmail.com>
2015-02-18 14:00:44 -0800

    ◉_◉

--- web/wp-includes/pluggable.php
+++ web/wp-includes/pluggable.php
@@ -528,7 +528,7 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 			//'bcc' => 'acquiremint-notifs@beachmint.com',
 			'subject' => $phpmailer->Subject,
 			'message' => $phpmailer->Body,
-			'type' => 'text',
+			'type' => 'html',
 			//'attachment' => WEBROOT.'/public-out/test.txt',
 		));
 		} catch (\Exception $e) { exit("$e"); }
