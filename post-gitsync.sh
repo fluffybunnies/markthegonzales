7323666bdfa74e2f359f6264f06a3134b3ccfea5
alec <alec.hulce@gmail.com>
2015-02-19 23:16:27 -0800

    ⊙▃⊙

--- post-gitsync.sh
+++ post-gitsync.sh
@@ -7,5 +7,7 @@ echo "post-gitsync.sh $dir $branch"
 chown -R www-data $dir/web/wp-content/uploads
 chown -R www-data $dir/web/wp-content/tmp_uploads
 chown -R www-data $dir/web/wp-content/plugins
+
+chown -R www-data $dir/web/wp-content
 # whyyy????...
-chown -R www-data $dir/web/wp-admin
+#chown -R www-data $dir/web/wp-admin
