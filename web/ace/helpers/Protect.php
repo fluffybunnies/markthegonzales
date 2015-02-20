3bcb496141fb1cf34976ba78a828ef08e2e3db9f
alec <alec.hulce@gmail.com>
2015-02-20 01:48:05 -0800

    (ʘ‿ʘ)

--- web/ace/helpers/Protect.php
+++ web/ace/helpers/Protect.php
@@ -17,35 +17,40 @@ class Protect extends HelperAbstract {
 			'bank' => 50,
 			'usePath' => true,
 		),$opts);
-		$key = md5($opts['usePath'] ? REQUEST_PATH : $_SERVER['REQUEST_URI']);
-		$fn = REPOROOT."/out/protect.preventBruteForce.$key";
-		if (is_file($fn)) {
-			$log = json_decode(file_get_contents($fn), true);
-			if (!is_array($log))
-				throw new \Exception('unexpected format');
-		} else {
-			$log = array();
+		try {
+			$key = md5($opts['usePath'] ? REQUEST_PATH : $_SERVER['REQUEST_URI']);
+			$fn = REPOROOT."/out/protect.preventBruteForce.$key";
+			if (is_file($fn)) {
+				$log = json_decode(file_get_contents($fn), true);
+				if (!is_array($log))
+					throw new \Exception('unexpected format');
+			} else {
+				$log = array();
+			}
+			$call = array(
+				't' => microtime(true),
+				'r' => $_SERVER['REQUEST_URI'],
+				'ip' => Ace::clientIp(),
+				's' => 1,
+			);
+			if (!empty($log)) {
+				$lastCall = end($log);
+				if (!is_array($lastCall) || !is_numeric(Ace::g($lastCall,'t')))
+					throw new \Exception('corrupted log file');
+				if ($call['t'] < $lastCall['t']+$opts['cap'])
+					$call['s'] = 0;
+			}
+			$log[] = $call;
+			if (($numLogs = count($log)) > $opts['bank'])
+				$log = array_splice($log, $numLogs-$opts['bank']);
+			file_put_contents($fn, json_encode($log));
+			if (!empty($_GET['debug'])) Ace::varDump($log);
+			if ($call['s'] == 0)
+				throw new \Exception('too many requests');
+		} catch (\Exception $e) {
+			echo $e->getMessage();
+			exit;
 		}
-		$call = array(
-			't' => microtime(true),
-			'r' => $_SERVER['REQUEST_URI'],
-			'ip' => Ace::clientIp(),
-			's' => 1,
-		);
-		if (!empty($log)) {
-			$lastCall = end($log);
-			if (!is_array($lastCall) || !is_numeric(Ace::g($lastCall,'t')))
-				throw new \Exception('corrupted log file');
-			if ($call['t'] < $lastCall['t']+$opts['cap'])
-				$call['s'] = 0;
-		}
-		$log[] = $call;
-		if (($numLogs = count($log)) > $opts['bank'])
-			$log = array_splice($log, $numLogs-$opts['bank']);
-		file_put_contents($fn, json_encode($log));
-		if (!empty($_GET['debug'])) Ace::varDump($log);
-		if ($call['s'] == 0)
-			throw new \Exception('too many requests');
 	}
 
 }
