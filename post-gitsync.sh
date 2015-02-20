fcd7ff56721c78f849cf06df1b48d88e2a49bff7
alec <alec.hulce@gmail.com>
2015-02-20 03:53:32 -0800

    (̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄

--- post-gitsync.sh
+++ post-gitsync.sh
@@ -6,30 +6,11 @@ echo "post-gitsync.sh $dir $branch"
 
 chown www-data $dir/web/public-out
 
-chown -R www-data $dir/web/wp-content/uploads
-chown -R www-data $dir/web/wp-content/tmp_uploads
-chown -R www-data $dir/web/wp-content/plugins
-chown www-data $dir/web/wp-content # let wp create a test file in this dir
+#chown -R www-data $dir/web/wp-content/uploads
+#chown -R www-data $dir/web/wp-content/tmp_uploads
+#chown -R www-data $dir/web/wp-content/plugins
+chown -R www-data $dir/web/wp-content # let wp create a test file in this dir
 chown www-data $dir/web/wp-admin/includes/file.php # wp compares owner of this file to a newly created one in get_filesystem_method()
-#chown -R www-data $dir/web/wp-admin/*
-
-
-# this works:
-# chown -R www-data /var/www/markthegonzales/web/wp-admin/*
-
-# this works:
-# 
-
-# this doesnt:
-# chown www-data /var/www/markthegonzales/web/wp-admin/*
-# chown -R www-data /var/www/markthegonzales/web/wp-admin
-# chown www-data /var/www/markthegonzales/web/wp-admin
-# find /var/www/markthegonzales/web/wp-admin/ | xargs chown www-data
-# find /var/www/markthegonzales/web/wp-admin -type d | xargs chown www-data
-# chown -R www-data /var/www/markthegonzales/web/wp-admin/* && chown -R root user && chown -R root network && chown -R root maint && chown -R root js && chown -R root includes && chown -R root images && chown -R root css
-# chown -R www-data /var/www/markthegonzales/web/wp-admin/* && find /var/www/markthegonzales/web/wp-admin/ | xargs chown -R root
-# chown -R www-data /var/www/markthegonzales/web/wp-admin/* && chown root /var/www/markthegonzales/web/wp-admin/*
-# chown -R www-data user && chown -R www-data network && chown -R www-data maint && chown -R www-data js && chown -R www-data includes && chown -R www-data images && chown -R www-data css
 
 # reset:
 # rm -fr /var/www/markthegonzales/web/wp-content/plugins/mailchimp-for-wp && chown -R root /var/www/markthegonzales/ && /root/sire/_common/gitsync.sh '/var/www/markthegonzales' 'master'
