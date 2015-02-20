6c20736679843405e09de99bad43c02139dfa72e
alec <alec.hulce@gmail.com>
2015-02-20 03:51:06 -0800

    ヽ༼° ͟ل͜ ͡°༽ﾉ

--- post-gitsync.sh
+++ post-gitsync.sh
@@ -6,9 +6,9 @@ echo "post-gitsync.sh $dir $branch"
 
 chown www-data $dir/web/public-out
 
-#chown -R www-data $dir/web/wp-content/uploads
-#chown -R www-data $dir/web/wp-content/tmp_uploads
-#chown -R www-data $dir/web/wp-content/plugins
+chown -R www-data $dir/web/wp-content/uploads
+chown -R www-data $dir/web/wp-content/tmp_uploads
+chown -R www-data $dir/web/wp-content/plugins
 chown www-data $dir/web/wp-content # let wp create a test file in this dir
 chown www-data $dir/web/wp-admin/includes/file.php # wp compares owner of this file to a newly created one in get_filesystem_method()
 #chown -R www-data $dir/web/wp-admin/*
