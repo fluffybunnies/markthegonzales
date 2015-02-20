e4ef36b237e64f36e760d61922330e5f092df4ec
alec <alec.hulce@gmail.com>
2015-02-19 23:54:26 -0800

    ¯(°_o)/¯

--- post-gitsync.sh
+++ post-gitsync.sh
@@ -8,5 +8,5 @@ chown -R www-data $dir/web/wp-content/uploads
 chown -R www-data $dir/web/wp-content/tmp_uploads
 chown -R www-data $dir/web/wp-content/plugins
 # why? possibly due to list_files()?
-chown -R www-data $dir/web/
+chown -R www-data $dir/web/*
 #chown -R www-data $dir/web/wp-admin/includes/file.php
