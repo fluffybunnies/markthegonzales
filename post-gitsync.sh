67a314284ed1445b43a364f53ce32b0e6ddd6a85
alec <alec.hulce@gmail.com>
2015-02-20 00:16:10 -0800

    ¯(°_o)/¯

--- post-gitsync.sh
+++ post-gitsync.sh
@@ -8,8 +8,11 @@ chown -R www-data $dir/web/wp-content/uploads
 chown -R www-data $dir/web/wp-content/tmp_uploads
 chown -R www-data $dir/web/wp-content/plugins
 # why?
-chown -R www-data $dir/web/wp-content
+#chown -R www-data $dir/web/wp-content
 #chown -R www-data $dir/web/wp-admin
 #chown -R www-data $dir/web/wp-includes
 
 #chown -R www-data $dir/web/wp-admin/includes/file.php
+
+# this works: chown -R www-data /var/www/markthegonzales/web/wp-admin/*
+# this doesnt: chown -R www-data /var/www/markthegonzales/web/wp-admin
