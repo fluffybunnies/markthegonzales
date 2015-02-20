7e8e48c1af5f2a137f73af890451ed6d6a14a135
alec <alec.hulce@gmail.com>
2015-02-20 03:22:51 -0800

    (⌐■_■)

--- web/wp-admin/includes/class-wp-upgrader.php
+++ web/wp-admin/includes/class-wp-upgrader.php
@@ -151,10 +151,10 @@ class WP_Upgrader {
 	 */
 	public function fs_connect( $directories = array(), $allow_relaxed_file_ownership = false ) {
 		global $wp_filesystem;
-		echo "wef";
-var_dump($directories[0]);
-var_dump($allow_relaxed_file_ownership);
-var_dump($this->skin->request_filesystem_credentials( false, $directories[0], $allow_relaxed_file_ownership ));
+		exit('WEFEWEFWEFWE');
+//var_dump($directories[0]);
+//var_dump($allow_relaxed_file_ownership);
+//var_dump($this->skin->request_filesystem_credentials( false, $directories[0], $allow_relaxed_file_ownership ));
 exit;
 		if ( false === ( $credentials = $this->skin->request_filesystem_credentials( false, $directories[0], $allow_relaxed_file_ownership ) ) ) {
 			return false;
