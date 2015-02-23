1b2879ca328feaf74330e5ef0d2977f0532c9243
alec <alec.hulce@gmail.com>
2015-02-22 23:12:13 -0800

    ლ(́◉◞౪◟◉‵ლ)

--- web/ace/misc/wordpress.php
+++ web/ace/misc/wordpress.php
@@ -17,6 +17,7 @@ function hook_wphead(){
 }
 
 function hook_wpfooter(){
+	echo '<script>if (!window.$ && window.jQuery) window.$ = window.jQuery;</script>';
 	echo '<script src="/ace.js" async></script>';
 	Tracking::pageView();
 }
